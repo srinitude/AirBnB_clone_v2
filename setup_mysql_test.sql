6b321468d6be5ede3710e17f05832ca9225dd67e
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-10-22 12:27:48 -0700

    adding mysql dev/test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- creates the database hbnb_test_db in MySQL server
+-- only if database doesn't already exist
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_p\
+wd';
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
