7533f977db2f09f70e6bd8d0cc6dc14fd68eef4d
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-10-22 20:52:06 -0700

    update user

--- models/user.py
+++ models/user.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
 """ holds class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """Representation of a user """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = 'users'
+
+    email =  Column(String(128), nullable=False)
+    password =  Column(String(128), nullable=False
+    first_name =  Column(String(128), nullable=False
+    last_name =  Column(String(128), nullable=False
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
