bea9474b659b7566d90756b990f518c1b53ca653
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-10-22 21:07:57 -0700

    update user

--- models/user.py
+++ models/user.py
@@ -9,9 +9,9 @@ class User(BaseModel, Base):
     __tablename__ = 'users'
 
     email =  Column(String(128), nullable=False)
-    password =  Column(String(128), nullable=False
-    first_name =  Column(String(128), nullable=False
-    last_name =  Column(String(128), nullable=False
+    password =  Column(String(128), nullable=False)
+    first_name =  Column(String(128), nullable=False)
+    last_name =  Column(String(128), nullable=False)
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
