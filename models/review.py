4082a9be6cb9aeef5062fb46030921d5cbf9ac43
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-10-22 21:18:15 -0700

    setup review

--- models/review.py
+++ models/review.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python
 """ holds class Review"""
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 
 class Review(BaseModel):
     """Representation of Review """
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = "reviews"
+
+    place_id = Column(String(60),
+                      ForeignKey"place_id",
+                      nullable=False)
+    user_id = Column(String(60),
+                      ForeignKey"user_id",
+                      nullable=False)
+    text = Column(String(1024),nullable=False)
 
     def __init__(self, *args, **kwargs):
         """initializes Review"""
