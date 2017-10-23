6ba4e779306035d1b579a389583311bb27dfca32
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-10-23 06:48:16 -0700

    rm float

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python
 """ holds class Amenity"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, Numeric, String, ForeignKey
+from sqlalchemy.orm import relationship
+import models
 
-
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """Representation of Amenity """
-    name = ""
+    if models.storage_type == "db":
+        __tablename__ = "amentities"
+
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place",
+                                       secondary=place_amenity,
+                                       backref="amenities")        )
+    else:
+        name = ""
 
     def __init__(self, *args, **kwargs):
         """initializes Amenity"""
