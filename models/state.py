a152a3c30bc01a9f45bac0e352ee4fc3bace23fb
Kiren <srinitude@gmail.com>
2017-11-14 20:25:50 +0000

    Adds change depending whether storage type is DB

--- models/state.py
+++ models/state.py
@@ -3,23 +3,29 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import models
 import os
 
 class State(BaseModel, Base):
     """Representation of state """
-    __tablename__ = "states"
 
-    name = Column(String(128),
-                  nullable=False)
     storage_type = os.getenv("HBNB_TYPE_STORAGE")
     if storage_type == "db":
+        __tablename__ = "states"
+
+        name = Column(String(128),
+                  nullable=False)
         cities = relationship("City",
                               backref="state",
                               cascade="delete")
     else:
+        name = ""
+
         @property
         def cities(self):
-            
+            objects = models.storage.all()
+            for object in objects:
+                
 
     def __init__(self, *args, **kwargs):
         """initializes state"""
