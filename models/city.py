8c9bf8eec3f9dc59b123d3e8f92cfc5e7f710a44
Kiren <srinitude@gmail.com>
2017-11-15 18:14:53 +0000

    Adds changes to place, user, and city

--- models/city.py
+++ models/city.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """ holds class City"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 import models
-import sqlalchemy
 
 class City(BaseModel, Base):
     """Representation of city """
@@ -14,6 +14,9 @@ class City(BaseModel, Base):
                   nullable=False)
         state_id = Column(String(60),
                           ForeignKey("states.id"))
+        places = relationship("Place",
+                              backref="cities",
+                              cascade="delete")
     else:
         name = ""
         state_id = ""
--- models/user.py
+++ models/user.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """ holds class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
 import models
 
 class User(BaseModel, Base):
@@ -13,6 +14,9 @@ class User(BaseModel, Base):
         password =  Column(String(128), nullable=False)
         first_name =  Column(String(128), nullable=True)
         last_name =  Column(String(128), nullable=True)
+        places = relationship("Place",
+                              backref="user",
+                              cascade="delete")
     else:
         email = ""
         password = ""
