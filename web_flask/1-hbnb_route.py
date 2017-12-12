41173f024e541682e745c0b5c1d2cc33b860654b
Kiren <srinitude@gmail.com>
2017-12-12 21:51:27 +0000

    Adds route for /hbnb

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+Start Flask app with root route
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """
+    Returns a string saying Hello HBNB
+    """
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    Returns a string saying HBNB
+    """
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
