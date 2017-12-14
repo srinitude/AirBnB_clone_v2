b41e8ffa255b7ece7634a6fa6099fbf56b357e33
Kiren <srinitude@gmail.com>
2017-12-14 00:27:26 +0000

    Gets DB storage to work with displaying various states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,1 +1,48 @@
 #!/usr/bin/python3
+"""
+Return list of available states either from file or DB storage
+"""
+from models import storage
+from flask import Flask, render_template
+import models
+
+
+class StateInfo:
+    """
+    Interface that template receives regardless of storage type
+    """
+    def __init__(self, id, name):
+        """
+        State ID and name
+        """
+        self.id = id
+        self.name = name
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_storage(exception):
+    """
+    Close file/DB storage on app teardown
+    """
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def display_states():
+    """
+    Displays all states
+    """
+    states = []
+    if models.storage_type == "db":
+        results = storage.all("State").values()
+        for result in results:
+            new_state = StateInfo(result.id, result.name)
+            states.append(new_state)
+    return render_template("7-states_list.html",
+                           states=states)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
